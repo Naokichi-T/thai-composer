9e4976e035ccc78805ee570a465c7e229aaf4074
naokichi <naokichi.tany@gmail.com>
2026-04-05 10:02:25 +0900

    スクロール量が0pxでもキーボードを閉じる

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -447,10 +447,10 @@
     isMobile = "ontouchstart" in window;
 
     if (isMobile) {
-      // スクロール量が10px以上のときだけキーボードを閉じる
+      // スクロール量が0px以上のときだけキーボードを閉じる
       // 少し触れただけでは反応しないようにするため
       window.addEventListener("scroll", () => {
-        if (window.scrollY > 10) {
+        if (window.scrollY > 0) {
           document.activeElement?.blur();
         }
       });
